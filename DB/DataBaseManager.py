99c0359e7f4f0b7ae33b0e0a2a5976cab4da6fb0
zzay <ampleenkov.do@gmail.com>
2025-10-19 01:23:42 +0300

    Refact init and add commit

--- DB/DataBaseManager.py
+++ DB/DataBaseManager.py
@@ -1,20 +1,28 @@
 from DB.models import Base, User, Robot, Product, InventoryHistory, AIPrediction
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+import logging
 
 class DataBaseManager:
-
-    def __init__(self, conn=None):
-        conn = "postgresql://warehouse_user:secure_password@localhost:5432/warehouse_db"
-        self.engine = create_engine(conn)
+    def __init__(self, conn_str: str):
+        self.conn_str = conn_str
+        self.engine = create_engine(conn_str, echo=False)
+        self.DBSession = sessionmaker(bind=self.engine)
         self.create_tables()
-        
-    
-    def create_tables(self):
-#        Base = declarative_base()
 
+    def create_tables(self):
         Base.metadata.create_all(self.engine)
         self.User = User
         self.Robot = Robot
         self.Product = Product
         self.InventoryHistory = InventoryHistory
         self.AIPrediction = AIPrediction
+
+    def _commit_record(self, record):
+        try:
+            with self.DBSession() as _s:
+                _s.add(record)
+                _s.commit()
+                logging.debug("Record added")
+        except Exception as e:
+            logging.error(f"Error adding record {e}")
